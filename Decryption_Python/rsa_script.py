78b0c75cfa66a45b878c4ad9f0c030eda1553dd1
ddr41g <coolerd2@gmail.com>
2023-06-26 23:14:15 +0530

    Updated

--- Decryption_Python/rsa_script.py
+++ Decryption_Python/rsa_script.py
@@ -1,22 +1,22 @@
 import rsa
 
 def load_key(key_loc):
-	key=None
+	private_key=None
 	with open(key_loc,'rb') as file:
-		key=rsa.PrivateKey.load_pkcs1(file.read(),format='PEM')
-	return key
+		private_key=rsa.PrivateKey.load_pkcs1(file.read(),format='PEM')
+	return private_key
 
-def decrypt_file(enc_data,key):
-	decrypted=rsa.decrypt(enc_data,key).decode('utf-8')
+def decrypt_file(enc_data,private_key):
+	decrypted=rsa.decrypt(enc_data,private_key).decode('utf-8')
 	return decrypted
 
 def run():
 	file_loc=input("Enter encrypted file location: ")
-	key_loc=input("Enter key location: ")
-	key=load_key(key_loc)
+	key_loc=input("Enter private key location: ")
+	private_key=load_key(key_loc)
 	with open(file_loc,'rb') as file:
 		enc_data=file.read()
-		dec_data=decrypt_file(enc_data,key)
+		dec_data=decrypt_file(enc_data,private_key)
 	print(f"Decryption: {dec_data}")
 
 if __name__ == '__main__':
